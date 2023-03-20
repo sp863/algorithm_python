88108f7a5ac79d02758dbe465bafcd23d6004d30
Scott <sp863429@gmail.com>
2023-03-21 00:23:36 +0900

    lvl0: 1 problem solved

--- programmers/lvl_0/85_등수매기기.py
+++ programmers/lvl_0/85_등수매기기.py
@@ -1,5 +1,19 @@
+# Solution
+
 def solution(score):
-    average_score = [(num[0] + num[1])//2 for num in score]
-    sorted_avg_score = 
-    print(score)
-    return [ for num in score]
+    rank = sorted([sum(s) / 2 for s in score], reverse=True)
+    rankDict = {}
+    for i, r in enumerate(rank):
+        if r not in rankDict.keys():
+            rankDict[r] = i + 1
+    return [rankDict[sum(s) / 2] for s in score]
+
+def solution(score):
+    a = sorted([sum(i) for i in score], reverse = True)
+    return [a.index(sum(i))+1 for i in score]
+
+from functools import reduce
+def solution(score):
+    sorted_score = sorted(reduce(lambda x,y:x.append(sum(y)) or x,score,[]),reverse=True)
+
+    return [sorted_score.index(sum(s))+1 for s in score]
