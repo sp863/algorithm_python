8af012241d56e2a888c51edece35ac7a52963026
Scott <sp863429@gmail.com>
2023-04-13 23:53:43 +0900

    lvl1: 1 problem solved

---
+++ programmers/lvl_1/57_다트게임/57_다트게임.py
@@ -0,0 +1,54 @@
+def solution(dartResult):
+    n = ''
+    score = []
+    for i in dartResult:
+        if i.isnumeric():
+            n += i
+        elif i == 'S':
+            n = int(n)**1
+            score.append(n)
+            n = ''
+        elif i == 'D':
+            n = int(n)**2
+            score.append(n)
+            n = ''
+        elif i == 'T':
+            n = int(n)**3
+            score.append(n)
+            n = ''
+        elif i == '*':
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else:
+                score[-1] = score[-1] * 2
+        elif i == '#':
+            score[-1] = score[-1] * -1
+
+    return sum(score)
+
+# Solution
+
+
+def solution(dartResult):
+    point = []
+    answer = []
+    dartResult = dartResult.replace('10', 'k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+    print(point)
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt:
+            answer[i] = answer[i] ** (sdt.index(j)+1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0:
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+    return sum(answer)
