00ab3dc50148be41346865129c08340421bf9003
Scott <sp863429@gmail.com>
2023-05-02 23:54:03 +0900

    lvl1: study solution

--- programmers/lvl_1/70_대충만든자판*/70_대충만든자판*.py
+++ programmers/lvl_1/70_대충만든자판*/70_대충만든자판*.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def solution(keymap, targets):
     # incorrect solution...
     answer = []
@@ -20,6 +23,29 @@ def solution(keymap, targets):
     return [-1] if len(found) <= 0 else answer
 
 # Solution
+
+
+def solution(keymap, targets):
+    answer = []
+
+    for target in targets:
+        tmp_num = 0
+        for t in target:
+            num_lst = []
+            for k in keymap:
+                try:
+                    num_lst.append(k.index(t))
+                except:
+                    num_lst.append(np.inf)
+
+            tmp_num += (min(num_lst)+1)
+
+        if tmp_num == np.inf:
+            answer.append(-1)
+        else:
+            answer.append(tmp_num)
+
+    return answer
 
 
 def solution(keymap, targets):
