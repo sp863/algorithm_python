ae2c74586fed7441d17afb358e7a3477f6dd018e
Scott <sp863429@gmail.com>
2023-05-05 22:23:32 +0900

    lvl1: study another incorrect solution

--- programmers/lvl_1/71_햄버거만들기*/71_햄버거만들기*.py
+++ programmers/lvl_1/71_햄버거만들기*/71_햄버거만들기*.py
@@ -24,5 +24,18 @@ def solution(ingredient):
             del s[-4:]
     return cnt
 
+
+def solution(ingredient):
+    s = []
+    cnt = 0
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            cnt += 1
+            s = s[:-4]  # 해당 부분으로 인해 시간 초과 발생
+    return cnt
+
 # this solution uses del instead of pop
 # need to understand why two arrays can be compared this way. Aren't they reference variables?
+# last solution is incorrect and can cause runtime error. why?
+# del s[-4:] is faster than s = s[:-4] because the latter creates a new array and assigns it to s
